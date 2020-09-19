a769412b788e3c372088b4914db7f4063d7e6ba6
pablo179 <castillo.morales.pablo.prog@gmail.com>
2020-09-18 21:40:47 -0500

    initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <div>
+        <img src="" alt="profileImage" />
+        <div>
+          <h1>Pablo David Castillo Morales</h1>
+          <p>
+            “Soy un programador en desarrollo, amante del codigo, los stickers,
+            el auto aprendizaje, los videojuegos y la innovación tecnológica”
+          </p>
+        </div>
+      </div>
+    </header>
+  );
+}
+export default Header;
