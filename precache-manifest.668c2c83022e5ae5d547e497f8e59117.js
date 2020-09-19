c19b47ae9dca4b8fa4eb05809187682a406a3a56
pablo179 <castillo.morales.pablo.prog@gmail.com>
2020-09-18 21:53:24 -0500

    Updates

---
+++ precache-manifest.668c2c83022e5ae5d547e497f8e59117.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6e342ca05aa1b770d0706da8cceb56a",
+    "url": "/myResume/index.html"
+  },
+  {
+    "revision": "631c165e5f8fd0c7673a",
+    "url": "/myResume/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "07dd4db920bd11223220",
+    "url": "/myResume/static/js/2.e34099fb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/myResume/static/js/2.e34099fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "631c165e5f8fd0c7673a",
+    "url": "/myResume/static/js/main.723beabb.chunk.js"
+  },
+  {
+    "revision": "dba7c0e9f0788c5e292f",
+    "url": "/myResume/static/js/runtime-main.a9742cb9.js"
+  }
+]);
